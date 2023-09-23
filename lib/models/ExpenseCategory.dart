5dd35508fc2321e7387d782cfba59fe96cb282a4
Satheesh C <satheesancchandran@gmail.com>
2023-09-23 14:12:42 +0530

    Change application packagename

--- lib/models/ExpenseCategory.dart
+++ lib/models/ExpenseCategory.dart
@@ -1,59 +1,45 @@
 import 'package:flutter/material.dart';
 
+class WhiteIcon extends Icon {
+  const WhiteIcon(IconData icon, {super.key})
+      : super(icon, color: Colors.white);
+}
+
 enum ExpenseCategory {
-  education(
-      "Education",
-      Icon(
-        Icons.menu_book_sharp,
-        color: Colors.white,
-      ),
-      Color(0xFFEF9A9A)),
-  healthcare("Healthcare", Icon(Icons.health_and_safety, color: Colors.white),
-      Color(0xFFA5D6A7)),
+  education("Education", WhiteIcon(Icons.menu_book_sharp), Colors.red),
+  healthcare("Healthcare", WhiteIcon(Icons.health_and_safety), Colors.green),
   sports(
     "Sports & Memberships",
-    Icon(Icons.sports_tennis, color: Colors.white),
-    Color(0xFF64B5F6),
+    WhiteIcon(Icons.sports_tennis),
+    Color(0xFF8D6E63),
   ),
-  vehicle("Vehicle & Transportation",
-      Icon(Icons.electric_bike, color: Colors.white), Color(0xFFF48FB1)),
-  bills("Utility Bills", Icon(Icons.library_books, color: Colors.white),
-      Color(0xFFFFB74D)),
-  gift("Gift purchases", Icon(Icons.card_giftcard, color: Colors.white),
-      Color(0xFFFFF176)),
-  shopping("Shopping", Icon(Icons.add_shopping_cart, color: Colors.white),
-      Color(0xFF80DEEA)),
-  groceries("Groceries & Grains",
-      Icon(Icons.local_grocery_store, color: Colors.white), Color(0xFF7986CB)),
-  food("Food & Beverages", Icon(Icons.emoji_food_beverage, color: Colors.white),
-      Colors.amber),
-  petcare("Pet care expense", Icon(Icons.pets, color: Colors.white),
-      Color(0xFF76FF03)),
-  childcare("Child care expense", Icon(Icons.child_care, color: Colors.white),
-      Color(0xFFBCAAA4)),
-  communication("Communication", Icon(Icons.phone, color: Colors.white),
-      Color(0xFF9575CD)),
-  entertainments(
-      "Entertainments", Icon(Icons.tv, color: Colors.white), Color(0xFFCE93D8)),
-  house("Other House needs", Icon(Icons.home, color: Colors.white),
-      Color(0xFFFF9E80)),
-  investments(
-      "Investments", Icon(Icons.money, color: Colors.white), Color(0xFFE57373)),
-  office("Office needs", Icon(Icons.local_post_office, color: Colors.white),
-      Color(0xFFFF7043)),
+  vehicle("Vehicle & Transportation", WhiteIcon(Icons.electric_bike),
+      Colors.yellow),
+  bills("Utility Bills", WhiteIcon(Icons.library_books), Colors.lime),
+  gift("Gift purchases", WhiteIcon(Icons.card_giftcard), Colors.purple),
+  shopping("Shopping", WhiteIcon(Icons.add_shopping_cart), Colors.orange),
+  groceries(
+      "Groceries & Grains", WhiteIcon(Icons.local_grocery_store), Colors.amber),
+  food("Food & Beverages", WhiteIcon(Icons.emoji_food_beverage), Colors.indigo),
+  petcare("Pet care expense", WhiteIcon(Icons.pets), Colors.cyan),
+  childcare("Child care expense", WhiteIcon(Icons.child_care), Colors.pink),
+  communication("Communication", WhiteIcon(Icons.phone), Colors.teal),
+  entertainments("Entertainments", WhiteIcon(Icons.tv), Colors.blue),
+  house("Other House needs", WhiteIcon(Icons.home), Colors.deepPurple),
+  investments("Investments", WhiteIcon(Icons.money), Colors.deepOrange),
+  office("Office needs", WhiteIcon(Icons.local_post_office), Colors.lightGreen),
   emi(
     "Monthly installment premium",
-    Icon(Icons.monetization_on, color: Colors.white),
-    Color(0xFFAB47BC),
+    WhiteIcon(Icons.monetization_on),
+    Color(0xFF4FC3F7),
   ),
   fruitsAndVegetables("Fruits & Vegetables",
-      Icon(Icons.energy_savings_leaf, color: Colors.white), Color(0xFFB388FF)),
-  fishAndMeat(
-      "Fish & Meat", Icon(Icons.fastfood, color: Colors.white), Colors.lime),
-  miscellaneous("Miscellaneous",
-      Icon(Icons.other_houses_outlined, color: Colors.white), Colors.lightBlue),
-  insurances("Insurance & Securities",
-      Icon(Icons.attach_money, color: Colors.white), Color(0xFFA5D6A7));
+      WhiteIcon(Icons.energy_savings_leaf), Color(0xFFB388FF)),
+  fishAndMeat("Fish & Meat", WhiteIcon(Icons.fastfood), Color(0xFFDCE775)),
+  miscellaneous("Miscellaneous", WhiteIcon(Icons.other_houses_outlined),
+      Color(0xFF1565C0)),
+  insurances("Insurance & Securities", WhiteIcon(Icons.attach_money),
+      Color(0xFFA5D6A7));
 
   final String qualifiedName;
   final Icon icon;
